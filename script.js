da169a8ef46cf7ca5a4931b1209af8bc5ff39d43
Grigory <39899404+GeraldRivsky@users.noreply.github.com>
2019-03-08 10:04:16 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+class HoverIntent {
+  constructor({elem, over, out}) {
+    this.elem = elem;
+    this.over = over;
+    this.out = out;
+    
+    this.addListeners();
+  }
+  addListeners() {
+    let self = this;
+    
+    this.elem.addEventListener('mouseleave', function() {
+      clearTimeout(self.slowId);
+      self.out();
+    });
+    
+    this.elem.addEventListener('mouseenter', function() {
+      new Promise(self.isSlowMove.bind(self)).then(self.over.bind(self));
+    });
+  }
+  isSlowMove(isSlow, isFast) {
+    this.slowId = setTimeout(isSlow, 500);
+    
+    setTimeout(isFast, 550);
+  }
+}
